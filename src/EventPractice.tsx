c3d72a258b14053372d9705de6d48ab1671ff68e
cothis <minho1030@naver.com>
2021-08-08 19:30:01 +0900

    feat: bind

--- src/EventPractice.tsx
+++ src/EventPractice.tsx
@@ -6,13 +6,20 @@ class EventPractice extends Component {
     message: '',
   };
 
+  constructor(props: any) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   handleChange(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({ message: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleClick() {
-    alert(this.state.message);
-    this.setState({ message: '' });
+    alert(this.state.username + ': ' + this.state.message);
+    this.setState({ username: '', message: '' });
   }
 
   render() {
@@ -21,8 +28,15 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
-          placeholder="아무거나 입력해보세요"
+          placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
         />
